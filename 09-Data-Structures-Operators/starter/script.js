8900ce6d9106cb393421950d2b55df3a66f2057c
mirmannan99 <mirmannan99@gmail.com>
2022-10-13 09:54:57 +0530

    finsihed data structures

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
 
@@ -374,3 +374,117 @@ const game = {
 // }
 
 //  Strings ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const checkMiddleSeat = function (seat) {
+//   const s = seat.slice(-1);
+//   if (s === 'B' || s === 'E') console.log('You got middle seat');
+//   else console.log('you got lucky');
+// };
+// checkMiddleSeat('11B');
+// checkMiddleSeat('23C');
+// checkMiddleSeat('3E');
+
+//  split and join+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const capitalizeName = function (name) {
+//   const capitalString = name.split(' ');
+//   const namesArr = [];
+//   console.log(capitalString);
+//   for (const x of capitalString) {
+//     // namesArr.push(x[0].toUpperCase() + x.slice(1));
+
+//     namesArr.push(x.replace(x[0], x[0].toUpperCase()));
+//   }
+//   console.log(namesArr);
+// };
+
+// capitalizeName('mannan is a legend');
+
+// PADDING ++++++++++++++++++++++++++++++++++++++++ ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const message = 'Go to gate 23';
+// console.log(message.padStart(25, '+'));
+
+// mask cresit card
+
+// const maskCreditCard = function (cardNum) {
+//   const str = cardNum + '';
+//   const last = str.slice(-4);
+//   const masked = last.padStart(str.length, 'x');
+//   console.log(masked);
+// };
+// maskCreditCard('8746547931215432');
+
+// Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+// The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+// THIS TEST DATA (pasted to textarea)
+// underscore_case
+//  first_name
+// Some_Variable
+//   calculate_AGE
+// delayed_departure
+
+// SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+// underscoreCase      ✅
+// firstName           ✅✅
+// someVariable        ✅✅✅
+// calculateAge        ✅✅✅✅
+// delayedDeparture    ✅✅✅✅✅
+
+// HINT 1: Remember which character defines a new line in the textarea 😉
+// HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+// HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+// HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+// Afterwards, test with your own test data!
+
+// GOOD LUCK 😀
+// */
+
+// /*
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
+
+// document.querySelector('button').addEventListener('click', function () {
+//   const text = document
+//     .querySelector('textarea')
+//     .value.toLowerCase()
+//     .trim()
+//     .split('_');
+
+//   // const text = 'mir_abdul_mannan'.split('_');
+
+//   const [first, ...rest] = text;
+
+//   const pascalCase = [first];
+//   for (const x of rest) {
+//     const finalword = x[0].toUpperCase() + x.slice(1);
+//     pascalCase.push(finalword.trim());
+//     // x.endsWith('e') ?? console.log('\n');
+//     // pascalCase.push(x.replace(x[0], x[0].toUpperCase()));
+//   }
+//   console.log(pascalCase);
+//   const camelCase = pascalCase.join('');
+//   console.log(camelCase);
+// });
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// console.log(flights.split('+'));
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(50);
+  console.log(output);
+}
