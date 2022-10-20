abfdf57f7b0ab01e6e9862adac789485dc6c6ffe
mirmannan99 <mirmannan99@gmail.com>
2022-10-20 12:13:04 +0530

    section 11

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,16 +61,186 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+    
+          <div class="movements__value">${mov}</div>
+    </div>    
+        `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-// LECTURES
+// + LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+/////////////////////////////////////////////
 
-/////////////////////////////////////////////////
+let arr = ['a', 'b', 'c', 'd', 'e'];
+let arr2 = ['f', 'g', 'h', 'i', 'j'];
+
+// console.log(arr.slice(2));
+// //*return new array from index 2
+// console.log(arr.slice(2, 4));
+// //*return new array from index 2 ends at 4
+
+// console.log(arr.slice(-1)); //* to get last element of array
+
+// console.log(arr.slice(1, -2)); //* trims last two and first one element
+
+// console.log(arr.slice()); //* gets copy of array
+
+// console.log([...arr]);
+
+// ? splice
+//! it mutates the original array
+
+// console.log(arr.splice(2)); //* it extracts the part of array and original array loses it
+// console.log(arr.splice(-1));
+// console.log(arr);
+// arr.splice(1, 2); //* got to index of one and then deletes two elements
+// console.log(arr);
+
+//? Reverse
+//! it mutates the original array and reverses it
+
+// console.log(arr.reverse()); //*  reverses the array
+// console.log(arr);
+
+//? Concat
+
+// const letters = arr.concat(arr2); //* merge two arrays
+// console.log(letters);
+
+// console.log([...arr, ...arr2]); //* merges two arrays
+
+//? Join
+
+// console.log(letters.join('-'));
+
+//? The new at method
+
+// const arNum = [23, 11, 64];
+
+// console.log(arr[0]);
+// console.log(arr.at(-1)); //* start from right side
+
+//? Loooping Arrays_forEach
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i}: You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i}: You withdrew ${Math.abs(movement)}`);
+//   }
+// }
+
+// console.warn('-----ForEach-----');
+
+// movements.forEach(function (mov, i, arr) {   //* array passes its arguments to this callback function parameters
+//   if (mov > 0) {
+//     console.log(`Movement ${i}: You deposited ${mov}`);
+//   } else {
+//     console.log(`Movement ${i}: You withdrew ${Math.abs(mov)}`);
+//   }
+// });
+
+//* usually used to iterate over arrays
+
+//? #6 for each method for maps and sets
+
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key} : ${value}`);
+// });
+
+// //- sets
+
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'AUD', 'USD']);
+
+// console.log(currenciesUnique);
+
+// currenciesUnique.forEach(function (value, key, map) {
+//   console.log(`${key} : ${value}`);
+// }); //* in sets the value and key is same basically sets doesn't have indexes also
+
+///////////////////////////////////////
+//+ Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+//! My answer
+
+// let julia = [3, 5, 2, 12, 7];
+// let kate = [4, 1, 15, 8, 3];
+// let juliaCopy = julia.slice(1, -2);
+// let bothDogs = [...juliaCopy, ...kate];
+// bothDogs.forEach(function (age, i) {
+//   if (age < 3) {
+//     console.log(
+//       `Dog number ${i + 1} is still a puppy 🐶, and is ${age} years old`
+//     );
+//   } else {
+//     console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`);
+//   }
+// });
+
+//! Jonas Answer
+
+// const checkDogs = function (dogsJulia, dogsKate) {
+//   const dogsJuliaCorrected = dogsJulia.slice();
+//   dogsJuliaCorrected.splice(0, 1);
+//   dogsJuliaCorrected.splice(-2);
+//   // console.warn(dogsJulia.slice(1, 3));
+//   const dogs = dogsJuliaCorrected.concat(dogsKate);
+
+//   dogs.forEach(function (age, i) {
+//     if (age < 3) {
+//       console.log(
+//         `Dog number ${i + 1} is still a puppy 🐶, and is ${age} years old`
+//       );
+//     } else {
+//       console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`);
+//     }
+//   });
+// };
+
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+
+//? #11 the Map Method
