bea3d9e5b0457ea03c22e91bf88565951cf16b57
mirmannan99 <mirmannan99@gmail.com>
2022-11-01 10:24:15 +0530

    almost finsihed 12

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,25 +61,176 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
     <div class="movements__row">
-          <div class="movements__type movements__type--${type}">${
-      i + 1
-    } ${type}</div>
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
     
-          <div class="movements__value">${mov}</div>
+    <div class="movements__value">${mov}₤</div>
     </div>    
-        `;
+    `;
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
 
-displayMovements(account1.movements);
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${acc.balance}€`;
+};
+
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}₤`;
+
+  const out = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}₤`;
+
+  const interest = acc.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter((int, i, arr) => {
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${Math.abs(interest)}₤`;
+};
+
+// ? #12 Computing Usernames
+
+// const user = 'Steven Thomas Williams';
+const createUsername = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner //!to add new property in the object with for each method+
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+  // const username = user
+  //   .toLowerCase()
+  //   .split(' ')
+  //   .map(name => name[0])
+  //   .join('');
+
+  // return username;
+};
+
+// createUsername('Mir Abdul Mannan');
+
+createUsername(accounts);
+
+const updateUI = function (acc) {
+  //Display Movements
+  displayMovements(acc.movements);
+  // Display Summary
+  calcDisplayBalance(acc);
+  // Display summary
+  calcDisplaySummary(acc);
+};
+
+/// Event handlers here
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+  //Prevent form from getting submit
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    //Display Ui and welcome message
+    labelWelcome.textContent =
+      'Welcome back, ' + currentAccount.owner.split(' ')[0];
+    containerApp.style.opacity = 100;
+    //clear the input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    //update UI
+    updateUI(currentAccount);
+  }
+});
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+  inputTransferAmount.value = inputTransferTo.value = '';
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc.username !== currentAccount.username
+  ) {
+    //Doing transfer
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+    //update UI
+    updateUI(currentAccount);
+  }
+});
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount / 10)) {
+    //ADD the movements
+
+    currentAccount.movements.push(amount);
+
+    //Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    //delete account
+    accounts.splice(index, 1);
+    console.log(accounts);
+
+    //Hide UI
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
+let sorted = false;
+
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
+
+// console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -188,22 +339,22 @@ let arr2 = ['f', 'g', 'h', 'i', 'j'];
 //+ Coding Challenge #1
 
 /* 
-Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
-
-Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
-
-1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
-2. Create an array with both Julia's (corrected) and Kate's data
-3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
-4. Run the function for both test datasets
-
-HINT: Use tools from all lectures in this section so far 😉
-
-TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
-TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
-
-GOOD LUCK 😀
-*/
+          Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+          
+          Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+          
+          1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+          2. Create an array with both Julia's (corrected) and Kate's data
+          3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+          4. Run the function for both test datasets
+          
+          HINT: Use tools from all lectures in this section so far 😉
+          
+          TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+          TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+          
+          GOOD LUCK 😀
+          */
 
 //! My answer
 
@@ -244,3 +395,249 @@ GOOD LUCK 😀
 // checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 
 //? #11 the Map Method
+const movements = account1.movements;
+
+// const eurtoUsd = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurtoUsd;
+//   // return 11;
+// }); //*returns new elements
+
+// const movementsUSD = movements.map(mov => mov * eurtoUsd); //* with arrow function
+
+// console.log(movementsUSD);
+
+// const movementsUSDfor = [];
+// for (const mov of movements) {
+//   movementsUSDfor.push(mov * eurtoUsd);
+// } //*with the help of loop
+
+// console.log(movementsUSDfor);
+
+// const movementsDescription = movements.map(
+//   (mov, i) =>
+//     `Movement ${i}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`
+// );
+
+// console.log(movementsDescription);
+
+// ? #13 The filter Method
+
+// const deposits = movements.filter(function (mov) {
+//   return mov > 0;
+// });
+
+// console.log(movements);
+
+// console.log(deposits);
+
+// const withdrawal = movements.filter(function (mov) {
+//   return mov < 0;
+// });
+
+// console.log(withdrawal);
+
+// ? #14 The reduce method
+
+//* acculmulator is like a snowball rolling down the hill
+
+// const balance = movements.reduce(function (acc, curr, i, arr) {
+//   console.log(`Iteration ${i} : ${acc}`);
+//   return acc + curr;
+// }, 0);
+
+// console.log(balance);
+
+//- Mximum value
+
+// const max = movements.reduce((acc, mov) => {
+//   if (acc > mov) return acc;
+//   else return mov;
+// }, movements[0]);
+
+// console.log(max);
+
+///////////////////////////////////////
+// ? Coding Challenge #2
+
+/* 
+          Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+          
+          Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+          
+          1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+          2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+          3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+          4. Run the function for both test datasets
+          
+          TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+          TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+          
+          GOOD LUCK 😀
+          */
+
+// const calcAverageHumanAge = function (ages) {
+//   const humanAge = ages.map(function (dogAge) {
+//     if (dogAge <= 2) {
+//       return 2 * dogAge;
+//     } else {
+//       return 16 + dogAge * 4;
+//     }
+//   });
+//   const adults = humanAge.filter(function (age) {
+//     return age >= 18;
+//   });
+//   const avg =
+//     adults.reduce(function (acc, age, i, arr) {
+//       // console.log(arr.length);
+//       return acc + age;
+//     }, 0) / adults.length;
+//   console.log(humanAge);
+//   console.log(adults);
+//   console.log(avg);
+//   return avg;
+// };
+
+// const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+
+// const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+// console.log(avg1, avg2);
+
+///////////////////////////////////////
+
+// ? #16 The Magic of chaining
+const eurtoUsd = 1.1;
+
+//+ pipeline
+
+// const totalDepositedUsd = movements
+//   .filter(mov => mov > 0)
+//   .map(mov => mov * eurtoUsd)
+//   .reduce((acc, mov) => acc + mov, 0);
+
+// const totalDepositedUsd = movements
+//   .filter(mov => mov < 0)
+//   .map((mov, i, arr) => {
+//     console.log(arr); //!inspecting the previous methods reults
+//     return mov * eurtoUsd;
+//   })
+//   .reduce((acc, mov) => acc + mov, 0);
+
+// console.log(totalDepositedUsd);
+
+// ? #17 Coding Challenge
+
+// Coding Challenge #3
+/* 
+          Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+          
+          TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+          TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+          
+          GOOD LUCK 😀
+          */
+//! with method chaining and without if else
+
+// const calcAverageHumanAge2 = function (ages) {
+//   return ages
+//     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+//     .filter(age => age >= 18)
+//     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+// };
+
+// const avg3 = calcAverageHumanAge2([5, 2, 4, 1, 15, 8, 3]);
+
+// const avg4 = calcAverageHumanAge2([16, 6, 10, 5, 6, 1, 4]);
+// console.log(avg3, avg4);
+
+//? #18 The Find Method
+
+// const firstWithdrawal = movements.find(mov => mov < 0);
+
+// console.log(movements);
+// console.log(firstWithdrawal);
+
+// console.log(accounts);
+// const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+
+// console.log(account);
+
+// ?  #22 some and every
+
+// //+some
+// console.log(movements);
+
+// console.log(movements.includes(-130));
+
+// const anyDeposits = movements.some(mov => mov > 500);
+// console.log(anyDeposits);
+
+// //+every
+
+// console.log(movements.every(mov => mov > 0));
+// console.log(account4.movements.every(mov => mov > 0));
+
+// ? #23 flat and flatmaps
+
+// + changes nested array into simple arrays
+
+// const arrr = [[1, 2, 3], [4, 5, 6], 7, 8];
+
+// const arrrDeep = arrr.flat();
+// console.log(arrrDeep);
+
+// const accountsMovements = accounts.map(acc => acc.movements);
+
+// console.log(accountsMovements);
+
+// const allMovements = accountsMovements.flat();
+// console.log(allMovements);
+
+// const overAllBal = allMovements.reduce((acc, mov) => acc + mov, 0);
+
+// console.log(overAllBal);
+
+// @ if we use chaining for the same method
+
+// const overAllBalance = accounts //! with the help of chaining
+//   .map(acc => acc.movements)
+//   .flat()
+//   .reduce((acc, mov) => acc + mov, 0);
+
+// console.log(overAllBalance);
+
+// ! we use map and flat all the time for that the new method was introduced called flatMap();
+
+// //+ flatmap()
+// const overAllBalanceFlatMap = accounts //! with the help of chaining and flatMap()
+//   .flatMap(acc => acc.movements)
+//   .reduce((acc, mov) => acc + mov, 0);
+
+// console.log(overAllBalanceFlatMap);
+
+// ? #24 Sorting Arrays
+
+//Strings
+// const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+// console.log(owners.sort());
+
+// //NUmbers
+
+// console.log(movements);
+// console.log(movements.sort());
+
+// ! sort method sorts in the form of string basically it treats every thing in the form of string either it is a number or anything
+
+// movements.sort((a, b) => {
+//   if (a > b) return 1; //! if number are in negative
+//   if (b > a) return -1;
+// });
+
+// console.log(movements);
+
+// ? #25 more ways of creating and filling Arrays
+
+const x = new Array(7);
+
+console.log(x);
--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,4 +3,353 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1;
+};
+
+const renderCountry = function (response, className = '') {
+  const html = ` <article class="country ${className}">
+          <img class="country__img" src="${response.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${response.name}</h3>
+            <h4 class="country__region">${response.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +response.population / 1000000
+            ).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${
+              response.languages[0].name
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              response.currencies[0].name
+            }</p>
+          </div>
+        </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.style.opacity = 1;
+};
+
 ///////////////////////////////////////
+
+// ? #3 asynchronous javascript
+
+// let p = `before callback`;
+// console.log(p);
+
+// setTimeout(function () {
+//   p = `Inside callback`; //! no code will wait for its execution it will executed in future
+//   console.log(p);
+// }, 5000);
+
+// p = 'after callback';
+// console.log(p);
+
+// ?4 AJAX
+
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
+
+//   request.addEventListener('load', function () {
+//     // console.log(this.responseText);
+//     const data = JSON.parse(this.responseText);
+
+//     const [response] = data;
+
+//     console.log(response);
+
+//     const html = ` <article class="country">
+//           <img class="country__img" src="${response.flag}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${response.name}</h3>
+//             <h4 class="country__region">${response.region}</h4>
+//             <p class="country__row"><span>👫</span>${(
+//               +response.population / 1000000
+//             ).toFixed(1)}</p>
+//             <p class="country__row"><span>🗣️</span>${
+//               response.languages[0].name
+//             }</p>
+//             <p class="country__row"><span>💰</span>${
+//               response.currencies[0].name
+//             }</p>
+//           </div>
+//         </article>`;
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+// getCountryData('portugal');
+
+// ? #6 welocme to callback hell
+
+// //! here we have one callback inside one another
+
+// const getCountryAndNeighbour = function (country) {
+//   //* ajax call country 1
+
+//   const request = new XMLHttpRequest();
+
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
+
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+
+//     //* render country data
+//     renderCountry(data);
+
+//     //* get neighbour country 2
+//     const [neighbour] = data.borders;
+//     if (!neighbour) return;
+
+//     //* ajax call country 2
+
+//     const request2 = new XMLHttpRequest();
+
+//     request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+
+//     request2.send();
+
+//     request2.addEventListener('load', function () {
+//       const data2 = JSON.parse(this.responseText);
+//       console.log(data2);
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
+
+// getCountryAndNeighbour('portugal');
+// getCountryAndNeighbour('usa');
+
+// ? #7 promises and fetch
+
+// const request = fetch(`https://restcountries.com/v2/name/portugal`);
+// console.log(request);
+
+// ? #8 Consuming Promises
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`) //!returning a promise
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json(); //! read data from response this itself return the promise
+//     })
+//     .then(function (data) {
+//       //! we call then method on that promise
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+//+ Simplified version of above code
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(response => response.json())
+//     .then(data => renderCountry(data[0]));
+// };
+
+// getCountryData('USA');
+
+// ? #9 Chaining Promises
+
+// const getCountryData = function (country) {
+//   //country 1
+
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(response => response.json())
+//     .then(data => {
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders[0];
+//       if (!neighbour) return;
+
+//       //country 2
+//       return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+//     })
+//     .then(response => response.json())
+//     .then(data => renderCountry(data, `neighbour`));
+// };
+// // !if we want to get data from the previous promise
+// //! always get then from returning the fetch
+// getCountryData('USA');
+
+// ? #10 Handling rejected errors
+
+// const getCountryData = function (country) {
+//   //country 1
+
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(
+//       response => response.json()
+//       // err => alert(err)
+//     )
+//     .then(data => {
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders[0];
+//       if (!neighbour) return;
+
+//       //country 2
+//       return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+//     })
+//     .then(
+//       response => response.json()
+//       // err => alert(err)
+//     )
+//     .then(data => renderCountry(data, `neighbour`))
+//     .catch(err => {
+//       console.log(`${err} 🎇🎇🎇🎇🎇🎇`);
+//       renderError(`Something went Wrong 🎇  ${err.message}. Try Again!!`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+// btn.addEventListener('click', function () {
+//   getCountryData('USA');
+// });
+
+// getCountryData('ddsfsa');
+
+// ? Throwing Errors manually
+
+// const getJSON = function (url, errorMSg = 'Something Went Wrong !') {
+//   return fetch(url).then(response => {
+//     if (!response.ok) throw new Error(`${errorMSg} ${response.status}`);
+//     //! to throw an self defined message/error to the catch()  method
+
+//     return response.json();
+//   });
+// };
+
+// const getCountryData = function (country) {
+//   //country 1
+//   getJSON(`https://restcountries.com/v2/name/${country}`, 'Country Not Found')
+//     .then(data => {
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders[0];
+
+//       if (!neighbour) throw new Error('No Neighbour Found!');
+
+//       //country 2
+//       return getJSON(
+//         `https://restcountries.com/v2/alpha/${neighbour}`,
+//         'Country Not Found'
+//       );
+//     })
+
+//     .then(data => renderCountry(data, `neighbour`))
+
+//     .catch(err => {
+//       renderError(`Something went Wrong 🎇  ${err.message}. Try Again!!`);
+//     })
+
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+// btn.addEventListener('click', function () {
+//   getCountryData('australia');
+// });
+
+// ? #13 Coding challenge
+
+///////////////Coding Challenge //////////////////////
+// do this after countries
+
+// ? #14 behind the scenes
+
+// ?16 creating simple promise
+
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   //*executer function
+
+//   console.log('Lottery draw is happening here 🔮🔮🔮🔮');
+
+//   setTimeout(function () {
+//     if (Math.random() >= 0.5) {
+//       resolve('You win the lottery 😄😄😄');
+//     } else {
+//       reject(`You Loose the Lottery😢😢😢`);
+//     }
+//   }, 1000);
+// });
+
+// lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// //* promisifying the set timeout
+// const wait = function (seconds) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
+
+// wait(2)
+//   .then(() => {
+//     console.log(`i waited for 1 sec`);
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log(`i waited for 2 second`);
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log(`i waited for 3 second`);
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log(`i waited for 4 second`);
+//     return wait(1);
+//   });
+
+// ? #17 promisfy geolocation API
+
+// const getPosition = function name(params) {
+//   return new Promise(function (resolve, reject) {
+//     // navigator.geolocation.getCurrentPosition(
+//     //   position => console.log(position),
+//     //   err => reject(err)
+//     // );
+
+//     //+ it is same as above but argument assign automatically
+
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// };
+
+// ? 19 consuming promises with async await
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+const whereAmI = async function (country) {
+  const pos = await getPosition();
+
+  const { latitude: lat, longitutde: lng } = pos.coords;
+
+  const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+
+  const dataGeo = await resGeo.json();
+
+  console.log(dataGeo);
+
+  const res = await fetch(`https://restcountries.com/v2/name/${country}`); //!it will first rsolve the first await promise and then rest of the code will be executed
+
+  // fetch(`https://restcountries.com/v2/name/${country}`).then(function (res) {
+  //   return console.log(res);
+  // }); //! this is same as above code
+
+  const data = await res.json();
+
+  renderCountry(data[0]);
+  console.log(data[0]);
+};
+whereAmI(`portugal`);
+console.log(`first`);
